e998521bad6dd3fb1cb2ab1d06b84ef29984451b
Sarath Kallatt Sivasankaran <kssarath2005@gmail.com>
2017-06-29 01:13:15 +0530

    Adding OTP request types - v1

--- src/main/scala/com/transport/domain/router/TransportRouter.scala
+++ src/main/scala/com/transport/domain/router/TransportRouter.scala
@@ -2,13 +2,19 @@ package com.transport.domain.router
 
 import akka.actor.{Actor, ActorLogging, Props}
 import akka.routing.FromConfig
+import com.transport.domain.Protocol.{TransportRequest, UnknownRequestException}
 import com.transport.domain.worker.TransportWorker
 
 class TransportRouter extends Actor with ActorLogging {
   private val router = context.actorOf(FromConfig.props(Props[TransportWorker]), "transport-router")
 
   override def receive: Receive = {
-    case msg => router.tell(msg, sender())
+    case req: TransportRequest => router.tell(req, sender())
+
+    case msg =>
+      val error = s"""Received unknown message ${msg}"""
+      log.error(error)
+      sender() ! UnknownRequestException(error)
   }
 }
 
--- src/main/scala/com/transport/domain/worker/TransportWorker.scala
+++ src/main/scala/com/transport/domain/worker/TransportWorker.scala
@@ -1,12 +1,23 @@
 package com.transport.domain.worker
 
-import akka.actor.{Actor, ActorLogging}
+import akka.actor.{Actor, ActorLogging, ActorRef}
+import com.transport.domain.Protocol.{TransportRequest, UnknownRequestException}
 
 class TransportWorker extends Actor with ActorLogging {
+  import TransportWorker.processRequest
+
   override def receive: Receive = {
-    case _ =>
-      val msg = s"""${self.path}: Received your (${sender()}) work"""
-      log.debug(msg)
-      sender() ! msg
+    case req: TransportRequest => processRequest(req, sender())
+
+    case msg =>
+      val error = s"""Received unknown message ${msg}"""
+      log.error(error)
+      sender() ! UnknownRequestException(error)
+  }
+}
+
+object TransportWorker {
+  def processRequest(request: TransportRequest, sender: ActorRef): Unit = {
+    //TODO: process the request
   }
 }
